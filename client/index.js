0ba73b417e7a75ffc7fc1172d5beb7c706b8d352
tessc14 <tessc14@gmail.com>
2023-01-17 12:50:54 +0000

    add goats

--- client/index.js
+++ client/index.js
@@ -1,5 +1,24 @@
 const herd = document.getElementById("herd");
 
+function createGoatCard(goat) {
+    //create a card
+    const card = document.createElement("div");
+
+    //add relevant class
+    card.classList.add("goat");
+
+    //add content
+    const header = document.createElement("h2");
+    header.textContent = goat["name"];
+    card.appendChild(header);
+
+    //customise classes
+    card.classList.add(goat["age"] < 5 ? "young" : "old");
+
+    //attach it to the container
+    herd.appendChild(card);
+}
+
 async function callTheHerd () {
 
     //request all goats from the API
@@ -8,8 +27,8 @@ async function callTheHerd () {
     //extract json data from the response
     const data = await response.json();
 
-    //log them
-    console.log(data);
+    data.forEach(g => createGoatCard(g));
+
 };
 
 callTheHerd()
